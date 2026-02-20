cc7da4ac864d1cab56514d1faa67f9e976fc538f
kprusty-338 <kprusty338@gmail.com>
2026-02-20 09:46:58 +0000

    this is a simple frontend project

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(){
+  const btn = document.getElementById('btn');
+  const countEl = document.getElementById('count');
+  let count = 0;
+  btn.addEventListener('click', () => {
+    count += 1;
+    countEl.textContent = `Button clicked: ${count}`;
+  });
+})();
